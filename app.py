6fb6a1c41fbaa929a2d755e29ed309d685e6e310
transelca-g5 <transelca-g5@gmail.com0>
2019-10-05 11:33:16 -0500

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask	
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
-@app.route("/")
-def main():
+Bootstrap(app)
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
